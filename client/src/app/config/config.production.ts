3dc1689a17a916eafef8ab1de6716f9abe566e85
Mark Prykhoda <m.prykhoda@gmail.com>
2019-03-28 15:15:31 +0200

    Login style enhancements, layout editting of all components

--- client/src/app/config/config.production.ts
+++ client/src/app/config/config.production.ts
@@ -1,7 +1,7 @@
 import { ConfigModel } from '@app/models/config.model';
 
 const config: ConfigModel = {
-	baseURL: 'https://therapy-08-03.herokuapp.com/'
-};
+	baseURL: 'https://ao-therapy.herokuapp.com/'
+}
 
 export default config;
